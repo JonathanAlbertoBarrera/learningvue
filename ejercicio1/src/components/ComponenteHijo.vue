c60c98df55c3249f3039b0d94dc894a5cf4b6a0d
JonathanAlbertoBarrera <20233tn110@utez.edu.mx>
2025-05-08 15:31:02 -0600

    Corrigiendo submódulo en ejercicio1

---
+++ ejercicio1/src/components/ComponenteHijo.vue
@@ -0,0 +1,25 @@
+<template>
+    <div :style="{backgroundColor: fondo, color:'white'}" :class="{'add-borde': borde}">
+    <p :style="{fontSize: tamLetra + 'px'}">{{txt}}</p>
+    <p v-if="txt===''">No has mandado nada</p>
+    </div>
+  
+  
+</template>
+
+<script>
+
+export default {
+  name: 'ComponenteHijo',
+  props: ['tamLetra','fondo','borde','txt']
+  
+}
+</script>
+
+<style >
+.add-borde {
+  border: 3px solid black;
+  padding: 10px;
+  border-radius: 8px;
+}
+</style>
---
+++ ejercicio1/src/components/ComponentePadre.vue
@@ -0,0 +1,37 @@
+<template>
+  <p>Soy componente padre</p>
+  <input v-model="tamano" type=number placeholder="ingresa un tamaño de letra"/>
+  <br>
+  <input v-model="txtEnviar" placeholder="envia algo"/>
+  <p v-if="tamano!=0">Tamaño a enviar: {{tamano}} </p>
+  <select v-model="color">
+    <option>Green</option>
+    <option>Blue</option>
+    <option>Red</option>
+  </select>
+  <p>Color a mandar: {{color}}</p>
+  
+  <input type=checkbox id="check" v-model="conBorde"/>
+  <label for="check">Borde</label>
+  <ComponenteHijo :tamLetra="tamano" :fondo="color" :borde="conBorde"  :txt="txtEnviar"/>
+</template>
+
+<script>
+import ComponenteHijo from './ComponenteHijo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ComponenteHijo
+  },
+  data(){
+    return{
+        tamano:0,
+        color: "yellow",
+        conBorde: false,
+        txtEnviar: ''
+    }
+  }
+}
+</script>
+
